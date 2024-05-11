4a6ba1acdffe767b806ee67f4bed46aa27dccf53
himanshu2351 <himanshu2351@gmail.com>
2024-05-12 04:10:57 +0530

    LinkedListNode class changes

--- Leetcode EASY/src/linkedList/LinkedListNode.java
+++ Leetcode EASY/src/linkedList/LinkedListNode.java
@@ -3,9 +3,15 @@ package linkedList;
 public class LinkedListNode<T> {
 	T data;
 	LinkedListNode<T> next;
+	LinkedListNode<T> prev;
+	LinkedListNode<T> child;
 	LinkedListNode<T> random;
 	public LinkedListNode(T data) 
     {
     	this.data = data;
+	}
+	public LinkedListNode(T data, LinkedListNode<T> next) {
+		this.data = data;
+		this.next = next;
 	}
 }
