d9c51b156c4742bdcc6a16488fda1f73c6071826
himanshu2351 <himanshu2351@gmail.com>
2025-10-18 22:39:04 +0530

    2235. Add Two Integers

---
+++ MAANG-PREP/AddTwoInteger.java
@@ -0,0 +1,18 @@
+/**
+ * 2235. Add Two Integers
+ * Given two integers num1 and num2, return the sum of the two integers.
+ *
+ * Example 1:
+ * Input: num1 = 12, num2 = 5
+ * Output: 17
+ *
+ * Example 2:
+ * Input: num1 = -10, num2 = 4
+ * Output: -6
+ */
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
